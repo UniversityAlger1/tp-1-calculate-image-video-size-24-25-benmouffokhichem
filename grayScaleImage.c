fdc31b9477288a06bb7e51fc072db791660ed98f
benmouffokhichem <150010271+benmouffokhichem@users.noreply.github.com>
2024-11-22 23:48:16 +0100

    Update grayScaleImage.c

--- grayScaleImage.c
+++ grayScaleImage.c
@@ -6,7 +6,18 @@
 //   grayScale size of the image Bitmap (in byte)
 float grayScaleImage(int w, int h) {
    // YOUR CODE HERE - BEGIN
+#include "config/grayScaleImage.h"
 
+// Parameters:
+//   w: width of the image
+//   h: height of the image
+// Return value
+//   grayScale size of the image Bitmap (in byte)
+float grayScaleImage(int w, int h) {
+    // Taille = largeur * hauteur
+    return (float)(w * h);
+   
+}
    // YOUR CODE HERE - END
    return 0;
 }
